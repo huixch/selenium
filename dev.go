aad6843c6c5695941544719a1d8800a40a5b9c23
惠先春 <huixch@163.com>
2023-06-19 15:12:35 +0800

    DevNull添加reader接口实现

--- dev.go
+++ dev.go
@@ -9,6 +9,12 @@ func (d *DevNull) Write(p []byte) (n int, err error) {
 	return
 }
 
+func (d *DevNull) Read(p []byte) (n int, err error) {
+	n = 0
+	err = nil
+	return
+}
+
 func NewDevNull() *DevNull {
 	return &DevNull{}
 }
